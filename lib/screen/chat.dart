eb5c82e5e55aa9db6936b1f1892a758728816b5c
ElVaquilla <ashley.cuevas889@gmail.com>
2025-01-15 20:55:24 +0100

    Cambio Estético

--- lib/screen/chat.dart
+++ lib/screen/chat.dart
@@ -1,3 +1,255 @@
+// Versión 2 ###########################################
+import 'package:bubble/bubble.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import '../models/chatModel.dart';
+import '../services/chat.dart';
+import '../services/user.dart';
+import '../services/storage.dart';
+import '../controllers/chatController.dart';
+import '../controllers/navigationController.dart';
+
+class ChatScreen extends StatefulWidget {
+  final String userId;
+  final String userName;
+
+  const ChatScreen({Key? key, required this.userId, required this.userName})
+      : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _messageController = TextEditingController();
+  late final ChatService chatService;
+  late UserService userService;
+  final ChatController chatController = Get.put(ChatController());
+  final NavigationController navController = Get.put(NavigationController());
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Inicializar servicios
+    userService = UserService();
+    chatService = ChatService(userService);
+    chatService.connect();
+    chatService.registerUser(StorageService.getId());
+    print("Socket conectado: ${chatService.socket?.connected}");
+
+    // Cargar mensajes iniciales
+    chatService.loadMessages(StorageService.getId()!);
+
+    // Marcar mensajes como leídos al entrar
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      navController.markMessagesAsRead();
+    });
+
+    // Listener para mensajes y eventos
+    chatService.socket?.on('load-messages-response', (data) {
+      List<Chat> messages = (data as List).map((item) {
+        return Chat(
+          receiver: item['receiver'],
+          sender: item['sender'],
+          message: item['message'],
+          date: DateTime.parse(item['date']),
+        );
+      }).toList();
+
+      // Filtrar mensajes duplicados
+      final existingMessages =
+          chatController.chatMessages.map((m) => m.message).toSet();
+      final newMessages =
+          messages.where((m) => !existingMessages.contains(m.message)).toList();
+
+      chatController.chatMessages.addAll(newMessages);
+    });
+
+    chatService.socket?.on('message-receive', (data) {
+      final message = Chat(
+        receiver: data['receiver'],
+        sender: data['sender'],
+        message: data['message'],
+        date: DateTime.parse(data['timestamp']),
+      );
+      chatController.chatMessages.insert(0, message);
+    });
+
+    chatService.socket?.on('typing', (_) {
+      chatController.isTyping.value = true;
+    });
+
+    chatService.socket?.on('stop-typing', (_) {
+      chatController.isTyping.value = false;
+    });
+  }
+
+  void _sendMessage() {
+    final text = _messageController.text.trim();
+    if (text.isNotEmpty) {
+      final timestamp = DateTime.now();
+      final chatMessage = Chat(
+        receiver: widget.userId,
+        sender: StorageService.getId(),
+        message: text,
+        date: timestamp,
+      );
+
+      // Agregar el mensaje localmente
+      chatController.chatMessages.insert(0, chatMessage);
+
+      // Enviar el mensaje al servidor
+      chatService.sendMessage(chatMessage.toJson());
+
+      // Limpiar el campo de texto
+      _messageController.clear();
+
+      // Detener el evento "typing"
+      chatService.sendStopTypingEvent(widget.userId);
+    }
+  }
+
+  Widget buildMessage(Chat message, bool isMe) {
+    final formattedTime = DateFormat('HH:mm').format(message.date);
+
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment:
+            isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          if (!isMe)
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/receiver_avatar.jpg"),
+              radius: 20,
+            ),
+          if (!isMe) const SizedBox(width: 10),
+          Flexible(
+            child: Bubble(
+              radius: Radius.circular(15.0),
+              color: isMe ? Colors.blueAccent : Colors.grey.shade200,
+              elevation: 0.0,
+              alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      isMe ? "Tú" : widget.userName,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: isMe ? Colors.white : Colors.black87),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      message.message,
+                      style: TextStyle(
+                        color: isMe ? Colors.white : Colors.black87,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        formattedTime,
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: isMe ? Colors.white70 : Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          if (isMe) const SizedBox(width: 10),
+          if (isMe)
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/user_avatar.jpg"),
+              radius: 20,
+            ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    chatService.disconnect();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.userName),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Obx(() => ListView.builder(
+                  reverse: true,
+                  itemCount: chatController.chatMessages.length,
+                  itemBuilder: (context, index) {
+                    final message = chatController.chatMessages[index];
+                    final isMe = message.sender == StorageService.getId();
+                    return buildMessage(message, isMe);
+                  },
+                )),
+          ),
+          Obx(() => chatController.isTyping.value
+              ? const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Escribiendo...",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                )
+              : const SizedBox.shrink()),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    onChanged: (value) {
+                      if (value.isNotEmpty) {
+                        chatService.sendTypingEvent(widget.userId);
+                      } else {
+                        chatService.sendStopTypingEvent(widget.userId);
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Escribe un mensaje...',
+                      filled: true,
+                      fillColor: Color.fromRGBO(220, 220, 220, 1),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send, color: Colors.blueAccent),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/* Versión 1 ###########################################
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import '../models/chatModel.dart';
@@ -215,3 +467,4 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+*/
--- lib/screen/chatbot.dart
+++ lib/screen/chatbot.dart
@@ -1,10 +1,183 @@
-
+// Seguda Versión ###############################################################
+import 'package:bubble/bubble.dart';
 import 'package:dialogflow_flutter/language.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:dialogflow_flutter/dialogflowFlutter.dart';
 import 'package:dialogflow_flutter/googleAuth.dart';
-  // Importar la librería dialogflow_flutter
+import 'package:intl/intl.dart';
+
+void main() => runApp(ChatBotApp());
+
+class ChatBotApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _messageController = TextEditingController();
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void response(String query) async {
+    try {
+      AuthGoogle authGoogle =
+          await AuthGoogle(fileJson: "assets/credenciales.json").build();
+      DialogFlow dialogflow =
+          DialogFlow(authGoogle: authGoogle, language: Language.spanish);
+      AIResponse aiResponse = await dialogflow.detectIntent(query);
+      String botMessage =
+          aiResponse.getListMessage()![0]["text"]["text"][0].toString();
+      setState(() {
+        messages.insert(0, {"data": 0, "message": botMessage});
+      });
+      print(botMessage);
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  Widget buildMessage(String message, int data) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment:
+            data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          data == 0
+              ? CircleAvatar(
+                  backgroundImage: AssetImage("assets/robot.png"),
+                  radius: 20,
+                )
+              : SizedBox.shrink(),
+          SizedBox(width: 10),
+          Flexible(
+            child: Bubble(
+              radius: Radius.circular(15.0),
+              color: data == 0
+                  ? Color.fromRGBO(23, 157, 139, 1)
+                  : Colors.orangeAccent,
+              elevation: 0.0,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  message,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 10),
+          data == 1
+              ? CircleAvatar(
+                  backgroundImage: AssetImage("assets/default.png"),
+                  radius: 20,
+                )
+              : SizedBox.shrink(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chatbot con Dialogflow"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 15, bottom: 10),
+            child: Text(
+              "Hoy, ${DateFormat("Hm").format(DateTime.now())}",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return buildMessage(messages[index]["message"],
+                    messages[index]["data"]);
+              },
+            ),
+          ),
+          Divider(
+            height: 5.0,
+            color: Colors.greenAccent,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: "Escribe un mensaje...",
+                      hintStyle: TextStyle(color: Colors.black26),
+                      filled: true,
+                      fillColor: Color.fromRGBO(220, 220, 220, 1),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide.none),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send, color: Colors.greenAccent),
+                  onPressed: () {
+                    String userMessage = _messageController.text;
+                    if (userMessage.isNotEmpty) {
+                      setState(() {
+                        messages.insert(
+                            0, {"data": 1, "message": userMessage});
+                      });
+                      _messageController.clear();
+                      response(userMessage);
+                    }
+                    FocusScope.of(context).unfocus();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+// Primera Versión ##############################################################
+/*
+import 'package:dialogflow_flutter/language.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:dialogflow_flutter/dialogflowFlutter.dart';
+import 'package:dialogflow_flutter/googleAuth.dart';
+// Importar la librería dialogflow_flutter
 
 void main() => runApp(ChatBotApp());
 
@@ -96,3 +269,4 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+*/
